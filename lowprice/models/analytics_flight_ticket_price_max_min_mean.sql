78be076cbc540e147d407aa77a58e7536ea1df33
JeonSangYong <tkddyd420@naver.com>
2024-06-14 16:48:39 +0900

    refactor: 최댓값 날짜와 최솟값 날짜  추가
    - 해당 항공권에서 가장 높았던 가격의 날짜와 가장 낮았던 가격의 날짜 컬럼 추가

--- lowprice/models/analytics_flight_ticket_price_max_min_mean.sql
+++ lowprice/models/analytics_flight_ticket_price_max_min_mean.sql
@@ -8,6 +8,44 @@ WITH base AS (
     JOIN flight_ticket_info fti
         ON ftph.id = fti.id
     GROUP BY fti.id, airport
+),
+
+max_price_dates AS (
+    SELECT
+        f.id,
+        f.price,
+        f.ts AS max_price_date
+    FROM
+        flight_ticket_price_history f
+    JOIN
+        base b ON f.id = b.id
+    WHERE
+        f.price = b.max_price
+),
+
+min_price_dates AS (
+    SELECT
+        f.id,
+        f.price,
+        f.ts AS min_price_date
+    FROM
+        flight_ticket_price_history f
+    JOIN
+        base b ON f.id = b.id
+    WHERE
+        f.price = b.min_price
 )
 
-SELECT * FROM base
+SELECT
+    b.id,
+    b.max_price,
+    md.max_price_date::DATE AS max_price_date,
+    b.min_price,
+    mind.min_price_date::DATE AS min_price_date,
+    b.mean_price
+FROM
+    base b
+JOIN
+    max_price_dates md ON b.id = md.id
+JOIN
+    min_price_dates mind ON b.id = mind.id;
