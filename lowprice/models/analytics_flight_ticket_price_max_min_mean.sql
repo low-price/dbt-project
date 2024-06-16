be25385555dfafba29368523e73d07ed8b5d4c96
JeonSangYong <tkddyd420@naver.com>
2024-06-16 20:34:29 +0900

    refactor: 항공권 대표 가격 지표
    - 총소요시간(total_duration) 컬럼 추가
    - [출발일 ~ 도착일] 컬럼 추가
    - 최대값, 최소값 중복 중에서 가장 최신의 컬럼 하나만 사용하도록 row_number 컬럼 추가
    - 컬럼별 별칭(alias) 추가
    - 항공타입 추가

--- lowprice/models/analytics_flight_ticket_price_max_min_mean.sql
+++ lowprice/models/analytics_flight_ticket_price_max_min_mean.sql
@@ -14,11 +14,14 @@ max_price_dates AS (
     SELECT
         f.id,
         f.price,
-        f.ts AS max_price_date
+        f.ts AS max_price_date,
+        (departure_duration + COALESCE(arrival_duration, INTERVAL '0 minutes')) AS total_duration,
+        TO_CHAR(departure_date, 'YYYY-MM-DD') || '~' || TO_CHAR(COALESCE(arrival_date, departure_date), 'YYYY-MM-DD') AS "출발일~도착일",
+        ROW_NUMBER() OVER (PARTITION BY f.id ORDER BY price DESC, ts DESC) AS row_number
     FROM
         flight_ticket_price_history f
-    JOIN
-        base b ON f.id = b.id
+    JOIN base b ON f.id = b.id
+    JOIN flight_ticket_info i ON i.id = b.id
     WHERE
         f.price = b.max_price
 ),
@@ -27,7 +30,8 @@ min_price_dates AS (
     SELECT
         f.id,
         f.price,
-        f.ts AS min_price_date
+        f.ts AS min_price_date,
+        ROW_NUMBER() OVER (PARTITION BY f.id ORDER BY price, ts DESC) AS row_number
     FROM
         flight_ticket_price_history f
     JOIN
@@ -38,14 +42,20 @@ min_price_dates AS (
 
 SELECT
     b.id,
-    b.max_price,
-    md.max_price_date::DATE AS max_price_date,
-    b.min_price,
-    mind.min_price_date::DATE AS min_price_date,
-    b.mean_price
-FROM
-    base b
-JOIN
-    max_price_dates md ON b.id = md.id
-JOIN
-    min_price_dates mind ON b.id = mind.id;
+    b.airport as "항공권",
+    md."출발일~도착일",
+    b.max_price as "최고가",
+    md.max_price_date::DATE AS "최고가 날짜",
+    b.min_price as "최저가",
+    mind.min_price_date::DATE AS "최저가 날짜",
+    b.mean_price as "평균가",
+    md.total_duration AS "총소요시간",
+    CASE
+        WHEN i.is_roundtrip = True THEN '왕복'
+        ELSE '편도'
+    END AS "항공타입"
+FROM base b
+JOIN flight_ticket_info i ON b.id = i.id
+JOIN max_price_dates md ON b.id = md.id AND md.row_number = 1
+JOIN min_price_dates mind ON b.id = mind.id AND mind.row_number = 1
+ORDER BY "항공타입", max_price, min_price;
